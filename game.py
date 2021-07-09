56c913082f380c0322f4dc2fb680ec8bdd8ce1c3
andersoneves <>
2021-07-09 16:56:06 -0400

    inclusão do reconhecedor de letras

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import time
 import pygame
 import os
 import json
 import random
+import libRec.libLibras as R
 
 # definindo cores
 BLACK = (0, 0, 0)
@@ -18,20 +19,22 @@ with open('data.json') as json_file:
 	 random.shuffle(data)
 	 objs=data
 print(len(objs))
+
 for i in range(0,len(objs)):
 	obj=data[i]["objeto"]
 	img=data[i]["img"]
 	qtd=data[i]["quantidade"]
 	texto=""
 	l=0
+	screen = pygame.display.set_mode((1720, 800))
+	# carregando fonte
+	font = pygame.font.SysFont(None, 150)
+
+	pygame.display.set_caption('Oque É?')
+
+	# preenchendo o fundo com azul
+
 	while True:
-		screen = pygame.display.set_mode((1720, 800))
-		# carregando fonte
-		font = pygame.font.SysFont(None, 150)
-
-		pygame.display.set_caption('Oque É?')
-
-		# preenchendo o fundo com preto
 		screen.fill(BLUE)
 		# desenhando na superfície
 		pygame.draw.line(screen,BLUE_LINE, [3, 600], [1200, 600], 6)
@@ -57,15 +60,26 @@ for i in range(0,len(objs)):
 
 		# atualizando a tela
 		pygame.display.flip()
+		letra=R.reconhece()
+		image=pygame.image.load(os.path.join('libRec/temp/',"img.png"))
+		image = pygame.transform.scale(image, (150, 150))
+		screen.blit(image, (1300, 600))
+		print(letra)
+
+		t = font.render(letra, True, WHITE)
+		screen.blit(t, [1500, 600])
+		if letra== obj[l]:
+			l+=1
+			texto+=letra
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
 				break
 			if event.type == pygame.KEYDOWN:
 				if event.unicode==obj[l]:
-					l=l+1
+					l+=1
 					texto+=event.unicode
 				print(event.unicode)
-				
+		pygame.display.update()
 		time.sleep(1)
 pygame.quit()
-
